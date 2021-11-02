003a55658d0897b98803148d4e5e6f4388f84231
G1veng <daniil-shishko@mail.ru>
2021-11-02 21:50:08 +0300

    Second way to find paths and test function

--- Graph/Form1.Designer.cs
+++ Graph/Form1.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Graph
 {
   partial class Form1
@@ -29,26 +31,27 @@ namespace Graph
     /// </summary>
     private void InitializeComponent()
     {
-      this.button1 = new System.Windows.Forms.Button();
+      this.createVertex = new System.Windows.Forms.Button();
       this.pictureBox1 = new System.Windows.Forms.PictureBox();
       this.button2 = new System.Windows.Forms.Button();
       this.textBox1 = new System.Windows.Forms.TextBox();
       this.button3 = new System.Windows.Forms.Button();
       this.label1 = new System.Windows.Forms.Label();
       this.button4 = new System.Windows.Forms.Button();
+      this.button1 = new System.Windows.Forms.Button();
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
       this.SuspendLayout();
       // 
-      // button1
+      // createVertex
       // 
-      this.button1.AllowDrop = true;
-      this.button1.Location = new System.Drawing.Point(41, 12);
-      this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(109, 42);
-      this.button1.TabIndex = 0;
-      this.button1.Text = "Создать врешину";
-      this.button1.UseVisualStyleBackColor = true;
-      this.button1.Click += new System.EventHandler(this.button1_Click);
+      this.createVertex.AllowDrop = true;
+      this.createVertex.Location = new System.Drawing.Point(41, 12);
+      this.createVertex.Name = "createVertex";
+      this.createVertex.Size = new System.Drawing.Size(109, 42);
+      this.createVertex.TabIndex = 0;
+      this.createVertex.Text = "Создать врешину";
+      this.createVertex.UseVisualStyleBackColor = true;
+      this.createVertex.Click += new System.EventHandler(this.createVertex_Click);
       // 
       // pictureBox1
       // 
@@ -62,7 +65,6 @@ namespace Graph
       // 
       // button2
       // 
-      this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.button2.Location = new System.Drawing.Point(41, 363);
       this.button2.Name = "button2";
       this.button2.Size = new System.Drawing.Size(129, 57);
@@ -73,7 +75,6 @@ namespace Graph
       // 
       // textBox1
       // 
-      this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
       this.textBox1.Location = new System.Drawing.Point(50, 183);
       this.textBox1.Name = "textBox1";
       this.textBox1.Size = new System.Drawing.Size(100, 22);
@@ -81,7 +82,6 @@ namespace Graph
       // 
       // button3
       // 
-      this.button3.Anchor = System.Windows.Forms.AnchorStyles.Left;
       this.button3.Location = new System.Drawing.Point(41, 290);
       this.button3.Name = "button3";
       this.button3.Size = new System.Drawing.Size(148, 67);
@@ -92,7 +92,6 @@ namespace Graph
       // 
       // label1
       // 
-      this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
       this.label1.AutoSize = true;
       this.label1.Location = new System.Drawing.Point(47, 163);
       this.label1.Name = "label1";
@@ -103,22 +102,32 @@ namespace Graph
       // 
       // button4
       // 
-      this.button4.Anchor = System.Windows.Forms.AnchorStyles.Left;
       this.button4.Location = new System.Drawing.Point(41, 217);
       this.button4.Name = "button4";
       this.button4.Size = new System.Drawing.Size(148, 67);
       this.button4.TabIndex = 6;
-      this.button4.Text = "Алгоритм Флойда-Уоршелла";
+      this.button4.Text = "Алгоритм Форда-Беллмана";
       this.button4.UseVisualStyleBackColor = true;
       this.button4.Click += new System.EventHandler(this.button4_Click);
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(41, 90);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(109, 44);
+      this.button1.TabIndex = 7;
+      this.button1.Text = "Тест";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Controls.Add(this.button4);
       this.Controls.Add(this.button1);
+      this.Controls.Add(this.button4);
+      this.Controls.Add(this.createVertex);
       this.Controls.Add(this.button2);
       this.Controls.Add(this.button3);
       this.Controls.Add(this.label1);
@@ -134,15 +143,17 @@ namespace Graph
 
     }
 
+
     #endregion
 
-    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button createVertex;
     private System.Windows.Forms.PictureBox pictureBox1;
     private System.Windows.Forms.Button button2;
     private System.Windows.Forms.TextBox textBox1;
     private System.Windows.Forms.Button button3;
     private System.Windows.Forms.Label label1;
     private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button1;
   }
 }
 
--- Graph/Form1.cs
+++ Graph/Form1.cs
@@ -18,90 +18,97 @@ namespace Graph
       InitializeComponent();
     }
 
-    //int findFastestToThis = 0;
+    public class Vertex
+    {
+    public int first;
+    public int second;
+    public int value;
+    }
+
     private static int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
+    {
+      int min = int.MaxValue;
+      int minIndex = 0;
+      for (int v = 0; v < verticesCount; ++v)
       {
-        int min = int.MaxValue;
-        int minIndex = 0;
-
-        for (int v = 0; v < verticesCount; ++v)
+        if (shortestPathTreeSet[v] == false && distance[v] <= min)
         {
-          if (shortestPathTreeSet[v] == false && distance[v] <= min)
-          {
-            min = distance[v];
-            minIndex = v;
-          }
+          min = distance[v];
+          minIndex = v;
         }
-
-        return minIndex;
       }
-
+      return minIndex;
+    }
 
     private static void Print(int[] distance, int verticesCount, int destination)
     {
-      
       for (int i = 1; i < verticesCount; ++i)
-        /*if(i == destination - 1)*/
-          MessageBox.Show("Вершина: " + (i + 1).ToString() + " Расстояние: " + distance[i].ToString());
+      {
+        MessageBox.Show("Вершина: " + (i + 1).ToString() + " Расстояние: " + distance[i].ToString());
+      }
     }
     public static void DijkstraAlgo(int[,] graph, int source, int verticesCount, int destination)
-      {
-        int[] distance = new int[verticesCount];
-        bool[] shortestPathTreeSet = new bool[verticesCount];
-
-        for (int i = 0; i < verticesCount; ++i)
-        {
-          distance[i] = int.MaxValue;
-          shortestPathTreeSet[i] = false;
-        }
-
-        distance[source] = 0;
-
-        for (int count = 0; count < verticesCount - 1; ++count)
-        {
-          int u = MinimumDistance(distance, shortestPathTreeSet, verticesCount);
-          shortestPathTreeSet[u] = true;
-
-          for (int v = 0; v < verticesCount; ++v)
-            if (!shortestPathTreeSet[v] && Convert.ToBoolean(graph[u, v]) && distance[u] != int.MaxValue && distance[u] + graph[u, v] < distance[v])
-              distance[v] = distance[u] + graph[u, v];
-        }
-
-      Print(distance, verticesCount, destination);
-      }
-
-    public void FloydWarshall_00(int[] matrix, int sz)
     {
-      for (var k = 0; k < sz; ++k)
+      int[] distance = new int[verticesCount];
+      bool[] shortestPathTreeSet = new bool[verticesCount];
+      for (int i = 0; i < verticesCount; ++i)
       {
-        for (var i = 0; i < sz; ++i)
+        distance[i] = int.MaxValue;
+        shortestPathTreeSet[i] = false;
+      }
+      distance[source] = 0;
+      for (int count = 0; count < verticesCount - 1; ++count)
+      {
+        int u = MinimumDistance(distance, shortestPathTreeSet, verticesCount);
+        shortestPathTreeSet[u] = true;
+
+        for (int v = 0; v < verticesCount; ++v)
         {
-          for (var j = 0; j < sz; ++j)
+          if (!shortestPathTreeSet[v] && Convert.ToBoolean(graph[u, v]) && distance[u] != int.MaxValue && distance[u] + graph[u, v] < distance[v])
           {
-            var distance = matrix[i * sz + k] + matrix[k * sz + j];
-            if (matrix[i * sz + j] > distance)
-            {
-              matrix[i * sz + j] = distance;
-            }
+            distance[v] = distance[u] + graph[u, v];
           }
         }
       }
-      for(int i = 0; i < buttonsCount; i++)
+      Print(distance, verticesCount, destination);
+    }
+
+    public void FordBellman(Vertex[] matrix, int sz)
+    {
+      int[] d = new int[buttonsCount];
+      for (int i = 0; i < buttonsCount; i++)
       {
-        if(matrix[i] != 0)
-          MessageBox.Show("Вершина: " + (i  + 1).ToString() + " Расстояние: " + matrix[i].ToString());
+        d[i] = 2000000000;
+      }
+      d[0] = 0;
+      for (int i = 1; i < buttonsCount; i++)
+      {
+        for (int j = 0; j < countOfLinks*2; j++)
+        {
+          if (d[matrix[j].second] > (d[matrix[j].first] + matrix[j].value))
+          {
+            d[matrix[j].second] = d[matrix[j].first] + matrix[j].value;
+          }
+          if (d[matrix[j].first] > (d[matrix[j].second] + matrix[j].value))
+          {
+            d[matrix[j].first] = d[matrix[j].second] + matrix[j].value;
+          }
+        }
+      }
+      for(int i = 1; i < buttonsCount; i++)
+      {
+        MessageBox.Show("Вершина: " + (i+1).ToString() + " Расстояние: " + d[i].ToString());
       }
     }
 
+    int countOfLinks = 0;
     List<List<int>> linksInLinks = new List<List<int>>();
-    List<int> links = new List<int>();
     Button first, second;
     bool seted = false;
     int choosedAll = 0;
-    Random rn = new Random();
     int buttonsCount = 0;
     List<Button> buttons = new List<Button>();
-    private void button1_Click(object sender, EventArgs e)
+    private void createVertex_Click(object sender, EventArgs e)
     {
       Graphics clear = pictureBox1.CreateGraphics();
       clear.Clear(Color.White);
@@ -114,7 +121,6 @@ namespace Graph
           {
             using (Graphics g = pictureBox1.CreateGraphics())
             {
-              //g.Clear(Color.White);
               Pen myPen = new Pen(Color.Red);
               int firstX = 0, firstY = 0;
               int secondX = 0, secondY = 0;
@@ -143,7 +149,6 @@ namespace Graph
               }
               Point point = new Point(theBiggestX, theBiggestY);
               g.DrawString(linksInLinks[i][j].ToString(), myFont, Brushes.Green, point);
-              //g.DrawLine(myPen, 0, 0, 1000, 1000);
             }
           }
         }
@@ -182,7 +187,6 @@ namespace Graph
     private void Form1_Load(object sender, EventArgs e)
     {
     }
-
     private void ButtonOnClick(object sender, EventArgs eventArgs)
     {
       Graphics clear = pictureBox1.CreateGraphics();
@@ -196,7 +200,6 @@ namespace Graph
           {
             using (Graphics g = pictureBox1.CreateGraphics())
             {
-              //g.Clear(Color.White);
               Pen myPen = new Pen(Color.Red);
               int firstX = 0, firstY = 0;
               int secondX = 0, secondY = 0;
@@ -207,7 +210,7 @@ namespace Graph
               myPen = new Pen(Color.Red);
               g.DrawLine(myPen, firstX, firstY, secondX, secondY);
               Font myFont = new Font("Arial", 14);
-              if (buttons[i].Location.X > second.Location.X)
+              if (buttons[i].Location.X > buttons[j].Location.X)
               {
                 theBiggestX = buttons[j].Location.X + (buttons[i].Location.X - buttons[j].Location.X) / 2;
               }
@@ -225,7 +228,6 @@ namespace Graph
               }
               Point point = new Point(theBiggestX, theBiggestY);
               g.DrawString(linksInLinks[i][j].ToString(), myFont, Brushes.Green, point);
-              //g.DrawLine(myPen, 0, 0, 1000, 1000);
             }
           }
         }
@@ -240,44 +242,32 @@ namespace Graph
           {
             if (seted)
             {
-              //if (second != null)
-              //{
-                //button.ForeColor = Color.Black;
-                if (buttons[i] != second && first != buttons[i])
-                {
-                  if (second != null)
-                    second.BackColor= Color.White;
-                  second = buttons[i];
-                  buttons[i].BackColor = Color.DeepSkyBlue;
-                  seted = false;
-                  choosedAll++;
-                }
-              //}
+              if (buttons[i] != second && first != buttons[i])
+              {
+                if (second != null)
+                  second.BackColor= Color.White;
+                second = buttons[i];
+                buttons[i].BackColor = Color.DeepSkyBlue;
+                seted = false;
+                choosedAll++;
+              }
             }
             else
             {
-              //if (first != null)
-              //{
-                if (buttons[i] != first && second != buttons[i])
-                {
-                  if(first != null)
-                    first.BackColor = Color.White;
-                  first = buttons[i];
-                  buttons[i].BackColor = Color.Orange;
-                  seted = true;
-                  choosedAll++;
-                }
-              //}
+              if (buttons[i] != first && second != buttons[i])
+              {
+                if(first != null)
+                  first.BackColor = Color.White;
+                first = buttons[i];
+                buttons[i].BackColor = Color.Orange;
+                seted = true;
+                choosedAll++;
+              }
             }
-            /*MessageBox.Show("Прощай " + button.Name);
-            button.Dispose();
-            buttonsCount--;*/
-            /*return button.Location;*/
           }
         }
       }
     }
-
     private void button3_Click(object sender, EventArgs e)
     {
       Graphics clear = pictureBox1.CreateGraphics();
@@ -291,7 +281,6 @@ namespace Graph
           {
             using (Graphics g = pictureBox1.CreateGraphics())
             {
-              //g.Clear(Color.White);
               Pen myPen = new Pen(Color.Red);
               int firstX = 0, firstY = 0;
               int secondX = 0, secondY = 0;
@@ -320,14 +309,12 @@ namespace Graph
               }
               Point point = new Point(theBiggestX, theBiggestY);
               g.DrawString(linksInLinks[i][j].ToString(), myFont, Brushes.Green, point);
-              //g.DrawLine(myPen, 0, 0, 1000, 1000);
             }
           }
         }
       }
       if (buttonsCount > 1)
       {
-        //findFastestToThis = int.Parse(textBox1.Text);
         int[,] graph = new int[buttonsCount, buttonsCount];
         for (int i = 0; i < buttonsCount; i++)
         {
@@ -336,37 +323,198 @@ namespace Graph
             graph[i, j] = linksInLinks[i][j];
           }
         }
-        DijkstraAlgo(graph, 0, linksInLinks.Count, int.Parse(textBox1.Text));
+        DijkstraAlgo(graph, 0, linksInLinks.Count, 1);
       }
       else
       {
         MessageBox.Show("Add some more vertexs");
       }
     }
-
     private void label1_Click(object sender, EventArgs e)
     {
 
     }
-
     private void button4_Click(object sender, EventArgs e)
     {
-      int[] matrix = new int[buttonsCount * buttonsCount]; 
+      int counter = 0;
+      Vertex[] matrix = new Vertex[(countOfLinks*2)]; 
       for(int i = 0; i < buttonsCount; i++)
       {
         for(int j = 0; j < buttonsCount; j++)
         {
-          matrix[i * buttonsCount + j] = linksInLinks[i][j];
+          if (linksInLinks[i][j] != 0)
+          {
+            matrix[counter] = new Vertex();
+            matrix[counter].first = i;
+            matrix[counter].second = j;
+            matrix[counter].value = linksInLinks[i][j];
+            counter++;
+          }
         }
       }
-      FloydWarshall_00(matrix, buttonsCount);
+      FordBellman(matrix, buttonsCount);
+    }
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (buttonsCount != 0)
+      {
+        for (int i = 0; i < buttonsCount; i++)
+        {
+          buttons[i].Dispose();
+        }
+        buttons.Clear();
+        linksInLinks.Clear();
+        buttonsCount = 0;
+        Graphics clear = pictureBox1.CreateGraphics();
+        clear.Clear(Color.White);
+      }
+      Random rnd = new Random();
+      buttonsCount = rnd.Next(3, 6);
+      int offsetX = 0, offsetY = 0;
+      int centerX = this.Width/2, CenterY = this.Height/2;
+      int currentX = 0, currentY = 0;
+      offsetX = 55;
+      offsetY = 55;
+      currentX = centerX + 46;
+      currentY = CenterY + 49;
+      for (int i = 0; i < buttonsCount; i++)
+      {
+        Button btn = new Button();
+        btn.Size = new Size(30, 30);
+        if(currentX < centerX && currentY < CenterY)
+        {
+          currentX = currentX + offsetX;
+          currentY = currentY - offsetY;
+          btn.Location = new Point(currentX, currentY);
+        }
+        if(currentX > centerX && currentY < CenterY)
+        {
+          currentX = currentX + offsetX;
+          currentY = currentY + offsetY;
+          btn.Location = new Point(currentX, currentY);
+        }
+        if (currentX > centerX && currentY > CenterY)
+        {
+          currentX = currentX - offsetX;
+          currentY = currentY + offsetY;
+          btn.Location = new Point(currentX, currentY);
+        }
+        if (currentX < centerX && currentY > CenterY)
+        {
+          currentX = currentX - offsetX;
+          currentY = currentY - offsetY;
+          btn.Location = new Point(currentX, currentY);
+        }
+        btn.Text = (i + 1).ToString();
+        btn.Name = i.ToString();
+        pictureBox1.Controls.Add(btn);
+        btn.BringToFront();
+        btn.BackColor = Color.White;
+        ControlExtension.Draggable(btn, true);
+        btn.Click += ButtonOnClick;
+        buttons.Add(btn);
+        string someString = btn.Name;
+      }
+      for (int i = 0; i < buttonsCount; i++)
+      {
+        linksInLinks.Add(new List<int>());
+        for (int j = 0; j < buttonsCount; j++)
+        {
+          linksInLinks[i].Add(0);
+        }
+      }
+      for (int k = 0; k < buttonsCount; k++)
+      {
+        for (int m = 0; m < buttonsCount; m++)
+        {
+          int createLink = rnd.Next(0, 5);
+          if (createLink > 2 || k == 0)
+          {
+            if (k != 0)
+            {
+              int Weight = rnd.Next(1, 6);
+              int first = rnd.Next(0, buttonsCount), second;
+              do
+              {
+                second = rnd.Next(0, buttonsCount);
+              }
+              while (second == first);
+              linksInLinks[first][second] = Weight;
+              linksInLinks[second][first] = Weight;
+            }
+            else
+            {
+              int Weight = rnd.Next(1, 6);
+              int first = 0, second;
+              do
+              {
+                second = rnd.Next(0, buttonsCount);
+              }
+              while (second == first);
+              linksInLinks[first][second] = Weight;
+              linksInLinks[second][first] = Weight;
+            }
+          }
+          int theBiggestY = 0, theBiggestX = 0;
+          for (int i = 0; i < buttonsCount; i++)
+          {
+            for (int j = i + 1; j < buttonsCount; j++)
+            {
+              if (linksInLinks[i][j] != 0)
+              {
+                using (Graphics g = pictureBox1.CreateGraphics())
+                {
+                  Pen myPen = new Pen(Color.Red);
+                  int firstX = 0, firstY = 0;
+                  int secondX = 0, secondY = 0;
+                  firstX = buttons[i].Location.X + buttons[i].Width;
+                  firstY = buttons[i].Location.Y + buttons[i].Height;
+                  secondX = buttons[j].Location.X + buttons[i].Width;
+                  secondY = buttons[j].Location.Y + buttons[i].Height;
+                  myPen = new Pen(Color.Red);
+                  g.DrawLine(myPen, firstX, firstY, secondX, secondY);
+                  Font myFont = new Font("Arial", 14);
+                  if (buttons[i].Location.X > buttons[j].Location.X)
+                  {
+                    theBiggestX = buttons[j].Location.X + (buttons[i].Location.X - buttons[j].Location.X) / 2;
+                  }
+                  else
+                  {
+                    theBiggestX = buttons[i].Location.X + (buttons[j].Location.X - buttons[i].Location.X) / 2;
+                  }
+                  if (buttons[i].Location.Y > buttons[j].Location.Y)
+                  {
+                    theBiggestY = buttons[j].Location.Y + (buttons[i].Location.Y - buttons[j].Location.Y) / 2;
+                  }
+                  else
+                  {
+                    theBiggestY = buttons[i].Location.Y + (buttons[j].Location.Y - buttons[i].Location.Y) / 2;
+                  }
+                  Point point = new Point(theBiggestX, theBiggestY);
+                  g.DrawString(linksInLinks[i][j].ToString(), myFont, Brushes.Green, point);
+                }
+              }
+            }
+          }
+        }
+      }
+      countOfLinks = 0; 
+      for (int i = 0; i < buttonsCount; i++)
+      {
+        for (int j = i + 1; j < buttonsCount; j++)
+        {
+          if(linksInLinks[i][j] != 0)
+          {
+            countOfLinks++;
+          }
+        }
+      }
     }
 
     private void button2_Click(object sender, EventArgs e)
     {
       Graphics clear = pictureBox1.CreateGraphics();
       clear.Clear(Color.White);
-      //pictureBox1.Image.Dispose();
       pictureBox1.InitialImage = null;
       if (buttonsCount > 1)
       {
@@ -375,10 +523,8 @@ namespace Graph
         int Width = buttons[0].Width / 2;
         int Height = buttons[0].Height / 2;
         Pen myPen = new Pen(Color.Red);
-        //if (choosedAll == 2) {
         using (Graphics g = pictureBox1.CreateGraphics())
         {
-          //g.Clear(Color.White);
           g.DrawLine(myPen, first.Location.X + Width, first.Location.Y + Height, second.Location.X + Width, second.Location.Y + Height);
           Font myFont = new Font("Arial", 14);
           if (first.Location.X > second.Location.X)
@@ -410,8 +556,8 @@ namespace Graph
           linksInLinks[int.Parse(first.Text) - 1][int.Parse(second.Text) - 1] = Weight;
           linksInLinks[int.Parse(second.Text) - 1][int.Parse(first.Text) - 1] = Weight;
           choosedAll = 0;
+          countOfLinks++;
         }
-        //}
       }
       else
       {
@@ -426,7 +572,6 @@ namespace Graph
           if(linksInLinks[i][j] != 0) {
             using (Graphics g = pictureBox1.CreateGraphics())
             {
-              //g.Clear(Color.White);
               Pen myPen = new Pen(Color.Red);
               int firstX = 0, firstY = 0;
               int secondX = 0, secondY = 0;
@@ -455,12 +600,10 @@ namespace Graph
               }
               Point point = new Point(theBiggestX, theBiggestY);
               g.DrawString(linksInLinks[i][j].ToString(), myFont, Brushes.Green, point);
-              //g.DrawLine(myPen, 0, 0, 1000, 1000);
             }
           }
         }
       }
     }
-
   }
 }
